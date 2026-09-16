3ec4f34db9ed5dc1aa4be51b77d82edcb279ce17
Anirban642 <anirbandas64237@gmail.com>
2026-09-16 13:33:37 +0530

    Ignore test scripts

--- Day - 10 (15.09.26)/todo-api/test.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST http://127.0.0.1:8000/categories/ -H "Content-Type: application/json" -d "{\"name\":\"Study\"}"
-
-curl -X POST http://127.0.0.1:8000/categories/ -H "Content-Type: application/json" -d "{\"name\":\"Work\"}"
-
-curl -X POST http://127.0.0.1:8000/todos/ -H "Content-Type: application/json" -d "{\"title\":\"Learn FastAPI\",\"description\":\"Practice CRUD\",\"category_id\":1}"
-
-curl -X POST http://127.0.0.1:8000/todos/ -H "Content-Type: application/json" -d "{\"title\":\"Learn Pydantic\",\"description\":\"Practice validation\",\"category_id\":1}"
-
-curl -X POST http://127.0.0.1:8000/todos/ -H "Content-Type: application/json" -d "{\"title\":\"Complete Assignment\",\"description\":\"Finish FastAPI assignment\",\"category_id\":2}"
